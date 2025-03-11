7c6f2e39c017ada0bca9d650ec0530b013ec5e7b
Robin <ikadekrobin2006@gmail.com>
2025-03-11 13:41:56 +0100

    Ticket 6

--- resources/views/pages/manual_list.blade.php
+++ resources/views/pages/manual_list.blade.php
@@ -5,25 +5,27 @@
     </x-slot:head>
 
     <x-slot:breadcrumb>
-        <li><a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/" alt="Manuals for '{{$brand->name}}'" title="Manuals for '{{$brand->name}}'">{{ $brand->name }}</a></li>
+        <li><a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/" alt="Manuals for '{{$brand->name}}'"
+                title="Manuals for '{{$brand->name}}'">{{ $brand->name }}</a></li>
     </x-slot:breadcrumb>
 
 
     <h1>{{ $brand->name }}</h1>
 
-    <p>{{ __('introduction_texts.type_list', ['brand'=>$brand->name]) }}</p>
+    <p>{{ __('introduction_texts.type_list', ['brand' => $brand->name]) }}</p>
 
-
+    <div class="wrap">
         @foreach ($manuals as $manual)
-
             @if ($manual->locally_available)
-                <a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/{{ $manual->id }}/" alt="{{ $manual->name }}" title="{{ $manual->name }}">{{ $manual->name }}</a>
+                <a href="{{route('manual.trackClick', $manual->id)}}" alt="{{ $manual->name }}" title="{{ $manual->name }}"
+                    class="manual-a">{{ $manual->name }}</a>
                 ({{$manual->filesize_human_readable}})
             @else
-                <a href="{{ $manual->url }}" target="new" alt="{{ $manual->name }}" title="{{ $manual->name }}">{{ $manual->name }}</a>
+                <a href="{{ route('manual.trackClick', $manual->id) }}" target="new" alt="{{ $manual->name }}"
+                    title="{{ $manual->name }}" class="manual-a">{{ $manual->name }}</a>
             @endif
-
             <br />
         @endforeach
+    </div>
 
 </x-layouts.app>
--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Manual:			/52/AEG/53/Superdeluxe/8023/manual/
 If we want to add product categories later:
 Productcat:		/category/12/Computers/
 */
-
+use Illuminate\Support\Collection;
 use App\Models\Brand;
 use App\Http\Controllers\RedirectController;
 use App\Http\Controllers\BrandController;
@@ -38,8 +38,13 @@ use App\Http\Controllers\LocaleController;
 Route::get('/', function () {
     $brands = Brand::all()->sortBy('name');
     $name = "Abdi";
-    return view('pages.homepage', compact('brands'), ["name"=>$name]);
+    $popularManuals = Brand::all()->sortBy('views');
+    return view('pages.homepage', compact('brands'), ["name"=> $name], ['popularManuals' => $popularManuals]);
 })->name('home');
+
+Route::get('/generateSitemap/', [SitemapController::class, 'generate']);
+
+Route::get('/manual/{id}/click', [ManualController::class, 'trackClick'])->name('manual.trackClick');
 
 Route::get('/manual/{language}/{brand_slug}/', [RedirectController::class, 'brand']);
 Route::get('/manual/{language}/{brand_slug}/brand.html', [RedirectController::class, 'brand']);
