eb5e849eb859b84f06f7e0f32e2580de82c3be8f
abdirahmanWeheliye <159246070+abdirahmanWeheliye@users.noreply.github.com>
2025-04-07 09:42:36 +0200

    ticket 13

---
+++ resources/views/pages/category.blade.php
@@ -0,0 +1,8 @@
+<x-layouts.app>
+    <h1>Categories</h1>
+    <li>
+        @foreach ($categories as $category)
+            <ul><a href="{{route('home', $category->id)}}">{{$category->name}}</a></ul>
+        @endforeach
+    </li>
+</x-layouts.app>
